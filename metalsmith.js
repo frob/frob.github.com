cdf969ac47d08120e5cc87a2b1a0bffe8a4d1355
Frank Anderson <frob@249517.no-reply.drupal.org>
2017-07-06 14:47:16 -0700

    added stock metalsmith js

---
+++ metalsmith.js
@@ -0,0 +1,31 @@
+var Metalsmith  = require('metalsmith');
+var collections = require('metalsmith-collections');
+var layouts     = require('metalsmith-layouts');
+var markdown    = require('metalsmith-markdown');
+var permalinks  = require('metalsmith-permalinks');
+const config = require('_config.yml');
+
+Metalsmith(__dirname)
+  .metadata({
+    sitename: "My Static Site & Blog",
+    siteurl: "http://example.com/",
+    description: "It's about saying »Hello« to the world.",
+    generatorname: "Metalsmith",
+    generatorurl: "http://metalsmith.io/"
+  })
+  .source('./src')            // source directory
+  .destination('./build')     // destination directory
+  .clean(true)                // clean destination before
+  .use(collections({          // group all blog posts by internally
+    posts: 'posts/*.md'       // adding key 'collections':'posts'
+  }))                         // use `collections.posts` in layouts
+  .use(markdown())            // transpile all md into html
+  .use(permalinks({           // change URLs to permalink URLs
+    relative: false           // put css only in /css
+  }))
+  .use(layouts({              // wrap layouts around html
+    engine: 'handlebars',     // use the layout engine you like
+  }))
+  .build(function(err) {      // build process
+    if (err) throw err;       // error handling is required
+  });
