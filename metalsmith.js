c114287498fb023acf6f58fa7efb23e8da1984f2
Frank Anderson <frob@249517.no-reply.drupal.org>
2017-07-21 12:59:57 -0700

    split collections off of the npm package to modify it

--- metalsmith.js
+++ metalsmith.js
@@ -15,6 +15,12 @@ const path = require('path');
 const consolodate = require('consolidate');
 const each = require('async').each;
 
+var extend = require('extend');
+var multimatch = require('multimatch');
+var unique = require('uniq');
+var read = require('fs').readFileSync;
+var loadMetadata = require('read-metadata').sync;
+
 function debug(logToConsole) {
   return function(files, metalsmith, done) {
     if (logToConsole) {
@@ -113,9 +119,6 @@ function jekyllFiles() {
 
         if (('collections' in metalsmith.metadata()) && ('posts' in metalsmith.metadata().collections)) {
           context._locals.site.posts = metalsmith.metadata().collections.posts;
-        }
-
-        if (('collections' in metalsmith.metadata()) && ('posts' in metalsmith.metadata().collections)) {
           context._locals.site.tags = metalsmith.metadata().collections;
         }
         context.onInclude(function (name, callback) {
@@ -209,27 +212,32 @@ ms = Metalsmith(__dirname)
     ])
     .destination('./_site')
     .use(jekylleCollections())
-    .use(collections({
+    .use(modCollections({
       posts: {
         pattern: '_posts/*.md',
         refer: false,
-        sortBy: 'date'
+        sortBy: 'date',
+        reverse: true
       },
       frontpage: {
         refer: false,
-        sortBy: 'date'
+        sortBy: 'date',
+        reverse: true
       },
       rants: {
         refer: false,
-        sortBy: 'date'
+        sortBy: 'date',
+        reverse: true
       },
       quarzack13: {
         refer: false,
-        sortBy: 'date'
+        sortBy: 'date',
+        reverse: true
       },
       tutorials: {
         refer: false,
-        sortBy: 'date'
+        sortBy: 'date',
+        reverse: true
       }
       // @TODO: add tags
       // @TODO: add category
@@ -269,3 +277,188 @@ ms = Metalsmith(__dirname)
         throw err;
       }
     });
+
+
+/**
+ * modules
+ */
+function modCollections(opts){
+  opts = normalize(opts);
+  var keys = Object.keys(opts);
+  var match = matcher(opts);
+
+  return function(files, metalsmith, done){
+    var metadata = metalsmith.metadata();
+
+    /**
+     * Find the files in each collection.
+     */
+
+    Object.keys(files).forEach(function(file){
+      debug('checking file: %s', file);
+      var data = files[file];
+console.log(files[file]);
+      data.url = data.path = file;
+
+      match(file, data).forEach(function(key){
+        if (key && keys.indexOf(key) < 0){
+          opts[key] = {};
+          keys.push(key);
+        }
+
+        metadata[key] = metadata[key] || [];
+        metadata[key].push(data);
+      });
+    });
+
+    /**
+     * Ensure that a default empty collection exists.
+     */
+
+    keys.forEach(function(key) {
+      metadata[key] = metadata[key] || [];
+    });
+
+    /**
+     * Sort the collections.
+     */
+
+    keys.forEach(function(key){
+      debug('sorting collection: %s', key);
+      var settings = opts[key];
+      var sort = settings.sortBy || 'date';
+      var col = metadata[key];
+
+      if ('function' == typeof sort) {
+        col.sort(sort);
+      } else {
+        col.sort(function(a, b){
+          a = a[sort];
+          b = b[sort];
+          if (!a && !b) return 0;
+          if (!a) return -1;
+          if (!b) return 1;
+          if (b > a) return -1;
+          if (a > b) return 1;
+          return 0;
+        });
+      }
+
+      if (settings.reverse) col.reverse();
+    });
+
+    /**
+     * Add `next` and `previous` references and apply the `limit` option
+     */
+
+    keys.forEach(function(key){
+      debug('referencing collection: %s', key);
+      var settings = opts[key];
+      var col = metadata[key];
+      var last = col.length - 1;
+      if (opts[key].limit && opts[key].limit < col.length) {
+          col = metadata[key] = col.slice(0, opts[key].limit);
+          last = opts[key].limit - 1;
+      }
+      if (settings.refer === false) return;
+      col.forEach(function(file, i){
+        if (0 != i) file.previous = col[i-1];
+        if (last != i) file.next = col[i+1];
+      });
+    });
+
+    /**
+     * Add collection metadata
+     */
+
+    keys.forEach(function(key){
+      debug('adding metadata: %s', key);
+      var settings = opts[key];
+      var col = metadata[key];
+      col.metadata = (typeof settings.metadata === 'string') ?
+        loadMetadata(settings.metadata) :
+        settings.metadata;
+    });
+
+    /**
+     * Add them grouped together to the global metadata.
+     */
+
+    metadata.collections = {};
+    keys.forEach(function(key){
+      return metadata.collections[key] = metadata[key];
+    });
+
+    done();
+  };
+}
+
+/**
+ * Normalize an `options` dictionary.
+ *
+ * @param {Object} options
+ */
+
+function normalize(options){
+  options = options || {};
+
+  for (var key in options) {
+    var val = options[key];
+    if ('string' == typeof val) options[key] = { pattern: val };
+    if (val instanceof Array) options[key] = { pattern: val };
+  }
+
+  return options;
+}
+
+/**
+ * Generate a matching function for a given set of `collections`.
+ *
+ * @param {Object} collections
+ * @return {Function}
+ */
+
+function matcher(cols){
+  var keys = Object.keys(cols);
+  var matchers = {};
+
+  keys.forEach(function(key){
+    var opts = cols[key];
+    if (!opts.pattern) {
+      return;
+    }
+    matchers[key] = {
+      match: function(file) {
+        return multimatch(file, opts.pattern)
+      }
+    };
+  });
+
+  return function(file, data){
+    var matches = [];
+
+    if (data.collection) {
+      var collection = data.collection;
+      if (!Array.isArray(collection)) {
+        collection = [collection];
+      }
+      collection.forEach(function(key){
+        matches.push(key);
+
+        if (key && keys.indexOf(key) < 0) {
+          debug('adding new collection through metadata: %s', key);
+        }
+      });
+    }
+
+    for (var key in matchers){
+      var m = matchers[key];
+      if (m.match(file).length) {
+        matches.push(key);
+      }
+    }
+
+    data.collection = unique(matches);
+    return data.collection;
+  };
+}
